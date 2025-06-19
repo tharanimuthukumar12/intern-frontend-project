f13dd95e01d070c43a51e609f11a3999b8794227
Tharani Muthukumar <tharanimuthukumar12@gmail.com>
2025-06-19 07:47:54 +0530

    Trigger redeploy

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Update from './Pages1/Update';
 import Edit from './Pages1/Edit';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// Triggering redeploy
 
 function App() {
   return (
